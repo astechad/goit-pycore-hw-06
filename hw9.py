37e2aeb4c1476dce175b255a7371c6af6a83f8c2
astechad <astech.ad@gmail.com>
2024-10-28 19:57:18 +0200

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,72 @@
+from collections import UserDict
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.validate(value):
+            raise ValueError("Phone number must contain exactly 10 digits")
+        super().__init__(value)
+
+    @staticmethod
+    def validate(phone_number):
+        return re.fullmatch(r"\d{10}", phone_number) is not None
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        new_phone = Phone(phone)
+        self.phones.append(new_phone)
+
+    def delete_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                return
+        raise ValueError("Phone number not found")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones_str = "; ".join(phone.value for phone in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def delete_record(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise ValueError(f"Record with name '{name}' not found")
+
+    def find_record(self, name):
+        return self.data.get(name, None)
